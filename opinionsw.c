baaa1adef2eb3320466f009af3a445ba3718fd50
Orahcio <orahcio@gmail.com>
2011-01-25 15:44:19 -0200

    opinionsw.c esta totalmente pronto para testes

--- opinionsw.c
+++ opinionsw.c
@@ -4,227 +4,63 @@
 #include <math.h>
 #include <gsl/gsl_rng.h>
 
-typedef struct rede {
+#define I (sizeof(int))
+#define Ip (sizeof(int *))
+#define D (sizeof(double))
 
-  int *st;
-
-  struct rede *prox;
-  struct rede *ante;
-
-} vertx;
-
-// Estrutura de parâmetros do modelo
 typedef struct parametros {
-  // Rede
-  int n, k, *s;
-  vertx **rede;
+  
   // Modelo de opinião
-  double p, eps, J, q;
+  double eps, J, q;
   int T_mcs, D_mcs;
-  // Sementes de números aleatórios
-  unsigned sem, sem_rede;
-  // Entrada e saída de dados
-  char out[255], fran[255];
+
 }par;
 
-int *alocavi(int n) {
-  
-  int *v;
-  
-  v=calloc(n,sizeof(int));
-  if(v==NULL) {
-    printf("Memoria insuficiente");
-    exit(-1);
-  }
-  
-  return (v);
-  
-}
+// Variaveis globais
+int n, K;
+double p;
 
-vertx **alocar_rede(int n) {
-  // ---------------------------------------
-  // Alocar a estrutura que abrigará a rede.
-  // ---------------------------------------
+void makesw(int **rede, unsigned long int sem) {
 
-  int i;
-  vertx **v;
+  // Constrói a rede small-world com base numa lista de vizinhança, em que o primeiro elemento é a conectividade do sítio
 
-  // Alocando:  
-  if(n<1) {
-    printf("\n**Parametros invalidos**\n");
-    exit(-1);
-  }
-  v=calloc(n,sizeof(vertx));
-  if(v==NULL) {
-    printf("Memoria insuficiente");
-    exit(-1);
-  }
-  for(i=0;i<n;i++) {
-    v[i]=NULL;
-    if(v==NULL) {
-      printf("Memoria insuficiente");
-      exit(-1);
-    }
-  }
-  // Fim da alocação de memória
+  int i, j, nex, nrand;
+  double ran;
+  gsl_rng *r;
 
-  return(v);
-
-}
-
-double *alocavd(int n) {
-  
-  double *v;
-  
-  v=calloc(n,sizeof(double));
-  if(v==NULL) {
-    printf("Memoria insuficiente");
-    exit(-1);
-  }
-  
-  return (v);
-  
-}
-
-void regular1D(vertx **rede, int *x, int n, int k) {
-  // -----------------------------------------------------------------
-  // Essa função retorna um vetor unidimensional de n nós para um anel 
-  // unidimensional com conectividade 2k.
-  // -----------------------------------------------------------------
-
-  int i, j;
-  vertx *novo, *p; // novo vertice e atual vertice
+  // iniciar semente
+  r=gsl_rng_alloc(gsl_rng_mt19937);
+  gsl_rng_set(r,sem); 
 
   for(i=0;i<n;i++) {
-    rede[i]=malloc(sizeof(vertx));
-    rede[i]->st=&x[i]; // Linka o endereço de estado
-    rede[i]->prox=NULL;
-    rede[i]->ante=NULL;
-    p=rede[i];
-    for(j=0;j<k;j++) { // ligações pra frente
-      //while(p->prox!=NULL) p=p->prox;
-      novo=malloc(sizeof(vertx));
-      novo->st=&x[(i+j+1)%n]; // a operação % coloca a condicao de contorno periodica
-      novo->prox=NULL;
-      novo->ante=p;
-      p->prox=novo;
-      p=p->prox; // salta pra proximo vertice
-    }
-    for(j=0;j<k;j++) { // ligações para trás
-      //while(p->prox!=NULL) p=p->prox;
-      novo=malloc(sizeof(vertx));
-      novo->st=&x[(n-k+i+j)%n]; // a operação % coloca a condicao de contorno periodica
-      novo->prox=NULL;
-      novo->ante=p;
-      p->prox=novo;
-      p=p->prox; // salta pra proximo vertice
-    }
-  }
-
-}
-
-void free_rede(vertx **rede, int n) {
-  // ---------------------------------
-  // Libera o espaço do vetor de rede.
-  // ---------------------------------
-
-  int i;
-
-  for(i=0;i<n;i++)
-    free(rede[i]);
-
-  free(rede);
-
-}
-
-void reagrupar(vertx **rede, int x[], int i, int j, int k) {
-  // ------------------------------------------------
-  // Essa função quebra a ligação i-j e constrói i-k.
-  // ------------------------------------------------
-
-  vertx *p, *novo;
-  int a=0;
-   
-  // se i-k ja existe não ocorre nada. Versão Nova!
-  for(p=rede[i];p!=NULL;p=p->prox)
-    if(p->st==&x[k]) return;
-
-  // no sitio i apenas o rotulo de j muda para apontar pra k
-  for(p=rede[i];p!=NULL;p=p->prox)
-    if(p->st==&x[j]) {
-      p->st=&x[k];
-      a=1;
-      break;
-    }
-  if(a==0) {
-    printf("%d-%d nao existe\n",i,j);
-    exit(-1);
-  }
-  a=0;
-
-  // no sitio j a ligacao com i é perdida
-  if(rede[j]->prox!=NULL) {
-    for(p=rede[j]->prox;p!=NULL;p=p->prox) {
-      if(p->st==&x[i]) {
-	if(p->ante!=NULL) (p->ante)->prox=p->prox;
-	if(p->prox!=NULL) (p->prox)->ante=p->ante;
-	free(p);
-	a=1;
-	break;
+    for(j=1;j<=K;j++) {
+      ran=gsl_rng_uniform(r);
+      if(ran<p) {
+	nrand=(i+K+1+gsl_rng_get(r)%(n-K-1))%n;
+	// Aloca para poder ter espaço, i-->nrand
+	rede[i][0]++; // Incrementa a conectividade
+	rede[i]=(int *)realloc(rede[i],(rede[i][0]+1)*I);
+	rede[i][rede[i][0]]=nrand;
+	// Aloca para poder ter espaço, nrand-->i
+	rede[nrand][0]++; // Incrementa a conectividade
+	rede[nrand]=(int *)realloc(rede[nrand],(rede[nrand][0]+1)*I);
+	rede[nrand][rede[nrand][0]]=i;
+	
       }
+      else
+	nex=(i+j)%n;
+	// Aloca para poder ter espaço, i-->nex
+	rede[i][0]++;
+	rede[i]=(int *)realloc(rede[i],(rede[i][0]+1)*I);
+	rede[i][rede[i][0]]=nex;
+	// Aloca para poder ter espaço, nex-->i
+	rede[nex][0]++;
+	rede[nex]=(int *)realloc(rede[nex],(rede[nex][0]+1)*I);
+	rede[nex][rede[nex][0]]=i;
     }
   }
-  if(a==0) {
-    printf("%d-%d nao existe\n",j,i);
-    exit(-1);
-  }
-  // k ganha a ligação de i
-  if(rede[k]->prox!=NULL) { // k ganha mais um vizinho
-    p=rede[k]->prox;
-    while(p->prox!=NULL) p=p->prox;
-    novo=malloc(sizeof(vertx));
-    novo->st=&x[i];
-    novo->prox=NULL;
-    novo->ante=p;
-    p->prox=novo;
-    p=p->prox;
-    }
 
-}
-
-void doSW(vertx **rede, int x[], int n, int k, double p, unsigned sem) {
-
-  int i, j, l;
-
-  //srand(234556431);
-  srand(sem);
-  for(i=0;i<n;i++)
-    for(j=0;j<k;j++) {
-      if(rand()/(double)RAND_MAX<p) {
-	//print_rede(rede,n); getchar();
-	l=(i+k+1+rand()%(n-k-1))%n;
-	reagrupar(rede,x,i,(i+j+1)%n,l);
-	//print_rede(rede,10); getchar();
-      }
-    }
-    
-}
-
-int *grau_vertices(vertx **rede, int n) {
-  // -------------------------------------
-  // Conta o grau de cada vértice da rede.
-  // -------------------------------------
-
-  int i, *k;
-  vertx *p;
-
-  k=alocavi(n);
-
-  for(i=0;i<n;i++)
-    for(p=rede[i]->prox;p!=NULL;p=p->prox)
-      k[i]++;
-
-  return(k);
+  gsl_rng_free(r);
 
 }
 
@@ -236,7 +72,11 @@ double *make_tau(int n, double J, double q, double epsilon) {
   int s, k;
   double *tau, h;
 
-  tau=alocavd((n+1)*(n+2)/2);
+  tau=(double *)calloc((n+1)*(n+2)/2,D);
+  if(tau==NULL) {
+    puts("Nao foi possivel alocar as taxas");
+    exit(-1);
+  }
 
   for(k=1;k<=n;k++)
     for(s=0;s<=k;s++) {
@@ -259,46 +99,31 @@ double *make_tau(int n, double J, double q, double epsilon) {
 
 }
 
-void salvar_estado(int n, int *state, FILE *out) {
-
-  int i;
-
-  for(i=0;i<n;i++)
-    fprintf(out, "%d\n", state[i]);
-
-}
-
-void opiniao(par *P) {
+void opiniao(int **rede, par *P, int s[], unsigned long int sem, unsigned long int sem_rede, char out[], char fran[]) {
   // ---------------------------------------------------
   // Aqui é a parte da simulação da dinâmica de opinião.
   // ---------------------------------------------------
 
   // Obtendo os parâmetros
-  int n=P->n, *k, k_max, *s=P->s, *h; // vetor de conectividade e com o estado dos sitios;
-  vertx **rede=P->rede;
+  int *h, k_max=0; // vetor de conectividade e com o estado dos sitios;
   double epsilon=P->eps, J=P->J, q=P->q;
-  int i, t, tt, T=P->T_mcs, nt=P->D_mcs; // passo monte-carlo e iterador de sítios da rede
-  unsigned long int nr; // Variável para receber o aleatório inteiro.
-  double *tau, prob, rho=0; // campo local de cada sítio e taca de transição
-  vertx *p; // iterador de sítios da rede
-  FILE *f=fopen(P->out,"w"), *g, *ff=fopen(P->fran,"r");
-  char out[255], fran[255];
+  int i, w, t, tt, T=P->T_mcs, nt=P->D_mcs; // passo monte-carlo e iterador de sítios da rede
+  double *tau, rho=0; // campo local de cada sítio e taca de transição
+  FILE *f=fopen(out,"w"), *g, *ff=fopen(fran,"r");
+  char ou[255];
   // Variáveis do gerador
   gsl_rng *r;
 
-  strcpy(out,P->out);
-  strcpy(fran,P->fran);
-  strcat(out,"~");
+  // Arquivo de saída de estado
+  strcat(ou,"~");
 
   // Iniciando a galera
-  h=alocavi(n);
-  k=grau_vertices(rede,n);
+  h=(int *)calloc(n,I);
   // Pegar o grau máximo
-  k_max=0;
   for(i=0;i<n;i++)
-    if(k[i]>k_max) k_max=k[i];
+    if(rede[i][0]>k_max) k_max=rede[i][0];
   
-  // Constrói as taxas com apenas o grau máximos
+  // Constrói as taxas de k=0 até o grau máximo
   tau=make_tau(k_max,J,q,epsilon);
 
   // Iniciando a semente de números aleatórios
@@ -314,32 +139,30 @@ void opiniao(par *P) {
 
   // O monte-carlo começa aqui
   for(t=0;t<T;t+=nt) {
-    g=fopen(out,"w");
+    g=fopen(ou,"w");
     ff=fopen(fran,"w");
-    fprintf(g,"# N=%d\tk=%d\tp=%lf\tsem=%lu\tJ=%lf\teps=%lf\tq=%lf\tSR=%u\n",n,P->k,P->p,nr,J,epsilon,q,P->sem_rede);
-    fprintf(g, "# Semente: %lu\n", nr);
+    fprintf(g,"# N=%d\tk=%d\tp=%lf\tsem=-1\tJ=%lf\teps=%lf\tq=%lf\tSR=%lu\n",n,K,p,J,epsilon,q,sem_rede);
+    fprintf(g, "# Semente: -1\n");
     for(tt=0;tt<nt;tt++) {
             
       // Calcular o vetor h dos sítios
       for(i=0;i<n;i++) {
 	h[i]=0;
-	for(p=rede[i]->prox;p!=NULL;p=p->prox) {
-	  h[i]+=*p->st;
+	for(w=1;w<=rede[i][0];w++) {
+	  h[i]+=s[rede[i][w]];
 	}
       }
 
       // Atualiza os sítios
       for(i=0;i<n;i++) {
-	nr=rand();
-	prob=gsl_rng_uniform(r);
-	if(prob<tau[h[i]-1+(k[i]+1)*k[i]/2]) {
+	if(gsl_rng_uniform(r)<tau[h[i]-1+(rede[i][0]+1)*rede[i][0]/2]) {
 	  if(s[i]==0) s[i]=1;
 	}
 	else
 	  if(s[i]==1) s[i]=0;
       }
 
-      // Calcula a densidade
+      // Calcula a densidade de estados
       rho=0;
       for(i=0;i<n;i++)
 	rho+=s[i];
@@ -349,7 +172,9 @@ void opiniao(par *P) {
       fprintf(f, "%lf\n", rho);
       
     }
-    salvar_estado(n,s,g);
+    // Salvando o estado
+    for(i=0;i<n;i++)
+      fprintf(g, "%d\n", s[i]);
     gsl_rng_fwrite(ff,r);
     fclose(g);
     fclose(ff);
@@ -357,7 +182,6 @@ void opiniao(par *P) {
   
   free(tau);
   free(h);
-  free(k);
   // Libera memória do gerador
   gsl_rng_free (r);
   
@@ -366,16 +190,15 @@ void opiniao(par *P) {
 int main(int argc, char *argv[]) {
 
   if(argc<6) {
-    //             1                2       3               4                 5
-    // 6 é um arquivo oculto .<saida densidade>
-    printf("Use %s <estado inicial> <T mcs> <N para salvar> <saida densidade> <arquivo com estado do gerador gsl>\n", argv[0]);
-    printf("Veja o cabeçalho do fonte para as definições dos parametros\n");
+    printf("Use %s <1. estado inicial> <2. T mcs> <3. T mcs para salvar> ",argv[0]);
+    printf("<4. TS para densidade> <5. bin do gerador>\n");
     exit(-1);
   }
 
-  int i, *s, si;
-  vertx **sw;
+  unsigned long int sem, sem_rede;
+  int i, **rede, *s, *hist;
   par P;
+  char out[255], fran[255];
   FILE *in=fopen(argv[1],"r");
 
   if(in==NULL) {
@@ -383,47 +206,53 @@ int main(int argc, char *argv[]) {
     exit(-1);
   }
 
-  // Coletando informações a partir do arquivo de dados
-  fscanf(in,"# N=%d\tk=%d\tp=%lf\tsem=%u\tJ=%lf\teps=%lf\tq=%lf\tSR=%u\n",&P.n,&P.k,&P.p,&P.sem,&P.J,&P.eps,&P.q,&P.sem_rede);
-  fscanf(in, "# Semente: %u\n", &P.sem);
+  // Variáveis da rede
+  p=strtod(argv[3],NULL);
+  n=atoi(argv[1]);
+  K=atoi(argv[2]);
 
-  // Coletando estado inicial
-  s=alocavi(P.n);
-  for(i=0;i<P.n;i++) {
+  // Coletando informações a partir do arquivo de estado inicial
+  fscanf(in,"# N=%d\tk=%d\tp=%lf\tsem=%lu\tJ=%lf\teps=%lf\tq=%lf\tSR=%lu\n",&n,&K,&p,&sem,&P.J,&P.eps,&P.q,&sem_rede);
+  fscanf(in, "# Semente: %lu\n", &sem);
+  P.T_mcs=atoi(argv[2]); P.D_mcs=atoi(argv[3]);
+
+  // Obtendo estado inicial
+  s=(int *)calloc(n,I);
+  for(i=0;i<n;i++) {
     fscanf(in,"%d",&s[i]);
   }
-
-  // Entrada a partir da linha de comando
-  P.D_mcs=atoi(argv[3]);
-  P.T_mcs=atoi(argv[2]);
-  strcpy(P.out,argv[4]);
-  strcpy(P.fran,argv[5]);
-  
   fclose(in);
 
-  // Mostra na tela os parâmetros do problema
-  //printf("\n# N=%d\tk=%d\tp=%lf\tsem=%d\tJ=%lf\teps=%lf\tq=%lf\tSR=%u\n",
-  //	  P.n,P.k,P.p,P.sem,P.J,P.eps,P.q,P.sem_rede);
+  // Nomes para saída do modelo e entrada do gerador
+  strcpy(out,argv[4]);
+  strcpy(fran,argv[5]);
 
-  // Montar substrato
-  sw=alocar_rede(P.n);
-  regular1D(sw,s,P.n,P.k);
-  doSW(sw,s,P.n,P.k,P.p,P.sem_rede);
+  // Alocando a estrutura de armazenamento da rede
+  rede=(int **)malloc(n*Ip);
+  for(i=0;i<n;i++) {
+    rede[i]=(int *)calloc(1,I);
+  }
 
-  P.rede=sw; // Apontar para a rede
-  P.s=s; // Estado da rede
+  makesw(rede,sem_rede);
 
-  //for(i=0;i<10;i++)
-  //  printf("%d ",P.s[i]);
-  //printf("\n");
-  
-  // Experimento
-  opiniao(&P);
 
-  free_rede(sw,P.n);
-  free(s);
-  
+  hist=(int *)calloc(n,I);
+  for(i=0;i<n;i++) {
+    hist[rede[i][0]]++;
+  }
+
+  for(i=0;i<n;i++)
+    if(hist[i]!=0) printf("%d %d\n",i,hist[i]);
+
+  for(i=0;i<n;i++) {
+    free(rede[i]);
+  }
+
+  free(rede);
+  free(hist);
+
+  //printf("A semente foi %lu.\n", sem);
+
   return 0;
-
 
 }
